b1dfcb6080204805b4196de8a956e58ecd584a9a
Hatice Nur Çakır <haticenurcakr@gmail.com>
2025-03-15 14:08:33 +0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,182 @@
+﻿namespace SistemPerformansTakibi
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+        private System.Windows.Forms.ProgressBar progressCpu;
+        private System.Windows.Forms.Label labelCpuUsage;
+        private System.Windows.Forms.ProgressBar progressRam;
+        private System.Windows.Forms.Label labelRamUsage;
+        private System.Windows.Forms.ProgressBar progressGpu;
+        private System.Windows.Forms.Label labelgGpuUsage;
+        private System.Windows.Forms.Label labelCpuTemp;
+        private System.Windows.Forms.Label labelGpuTemp;
+        private System.Windows.Forms.Label labelDiskUsage;
+        private LiveCharts.WinForms.CartesianChart cartesianChart1;
+        private LiveCharts.WinForms.CartesianChart cartesianChart2;
+        private LiveCharts.WinForms.CartesianChart cartesianChart3;
+        private LiveCharts.WinForms.CartesianChart cartesianChart4;
+        private LiveCharts.WinForms.CartesianChart cartesianChart5;
+        private LiveCharts.WinForms.CartesianChart cartesianChart6;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            this.progressCpu = new System.Windows.Forms.ProgressBar();
+            this.labelCpuUsage = new System.Windows.Forms.Label();
+            this.progressRam = new System.Windows.Forms.ProgressBar();
+            this.labelRamUsage = new System.Windows.Forms.Label();
+            this.progressGpu = new System.Windows.Forms.ProgressBar();
+            this.labelgGpuUsage = new System.Windows.Forms.Label();
+            this.labelCpuTemp = new System.Windows.Forms.Label();
+            this.labelGpuTemp = new System.Windows.Forms.Label();
+            this.labelDiskUsage = new System.Windows.Forms.Label();
+            this.cartesianChart1 = new LiveCharts.WinForms.CartesianChart();
+            this.cartesianChart2 = new LiveCharts.WinForms.CartesianChart();
+            this.cartesianChart3 = new LiveCharts.WinForms.CartesianChart();
+            this.cartesianChart4 = new LiveCharts.WinForms.CartesianChart();
+            this.cartesianChart5 = new LiveCharts.WinForms.CartesianChart();
+            this.cartesianChart6 = new LiveCharts.WinForms.CartesianChart();
+            this.SuspendLayout();
+            // 
+            // progressCpu
+            // 
+            this.progressCpu.Location = new System.Drawing.Point(0, 0);
+            this.progressCpu.Name = "progressCpu";
+            this.progressCpu.Size = new System.Drawing.Size(100, 23);
+            this.progressCpu.TabIndex = 0;
+            // 
+            // labelCpuUsage
+            // 
+            this.labelCpuUsage.Location = new System.Drawing.Point(0, 0);
+            this.labelCpuUsage.Name = "labelCpuUsage";
+            this.labelCpuUsage.Size = new System.Drawing.Size(100, 23);
+            this.labelCpuUsage.TabIndex = 1;
+            // 
+            // progressRam
+            // 
+            this.progressRam.Location = new System.Drawing.Point(0, 0);
+            this.progressRam.Name = "progressRam";
+            this.progressRam.Size = new System.Drawing.Size(100, 23);
+            this.progressRam.TabIndex = 2;
+            // 
+            // labelRamUsage
+            // 
+            this.labelRamUsage.Location = new System.Drawing.Point(0, 0);
+            this.labelRamUsage.Name = "labelRamUsage";
+            this.labelRamUsage.Size = new System.Drawing.Size(100, 23);
+            this.labelRamUsage.TabIndex = 3;
+            // 
+            // progressGpu
+            // 
+            this.progressGpu.Location = new System.Drawing.Point(0, 0);
+            this.progressGpu.Name = "progressGpu";
+            this.progressGpu.Size = new System.Drawing.Size(100, 23);
+            this.progressGpu.TabIndex = 4;
+            // 
+            // labelgGpuUsage
+            // 
+            this.labelgGpuUsage.Location = new System.Drawing.Point(0, 0);
+            this.labelgGpuUsage.Name = "labelgGpuUsage";
+            this.labelgGpuUsage.Size = new System.Drawing.Size(100, 23);
+            this.labelgGpuUsage.TabIndex = 5;
+            // 
+            // labelCpuTemp
+            // 
+            this.labelCpuTemp.Location = new System.Drawing.Point(0, 0);
+            this.labelCpuTemp.Name = "labelCpuTemp";
+            this.labelCpuTemp.Size = new System.Drawing.Size(100, 23);
+            this.labelCpuTemp.TabIndex = 6;
+            // 
+            // labelGpuTemp
+            // 
+            this.labelGpuTemp.Location = new System.Drawing.Point(0, 0);
+            this.labelGpuTemp.Name = "labelGpuTemp";
+            this.labelGpuTemp.Size = new System.Drawing.Size(100, 23);
+            this.labelGpuTemp.TabIndex = 7;
+            // 
+            // labelDiskUsage
+            // 
+            this.labelDiskUsage.Location = new System.Drawing.Point(0, 0);
+            this.labelDiskUsage.Name = "labelDiskUsage";
+            this.labelDiskUsage.Size = new System.Drawing.Size(100, 23);
+            this.labelDiskUsage.TabIndex = 8;
+            // 
+            // cartesianChart1
+            // 
+            this.cartesianChart1.Location = new System.Drawing.Point(12, 266);
+            this.cartesianChart1.Name = "cartesianChart1";
+            this.cartesianChart1.Size = new System.Drawing.Size(370, 170);
+            this.cartesianChart1.TabIndex = 9;
+            this.cartesianChart1.ChildChanged += new System.EventHandler<System.Windows.Forms.Integration.ChildChangedEventArgs>(this.cartesianChart1_ChildChanged);
+            // 
+            // cartesianChart2
+            // 
+            this.cartesianChart2.Location = new System.Drawing.Point(392, 29);
+            this.cartesianChart2.Name = "cartesianChart2";
+            this.cartesianChart2.Size = new System.Drawing.Size(316, 170);
+            this.cartesianChart2.TabIndex = 10;
+            this.cartesianChart2.ChildChanged += new System.EventHandler<System.Windows.Forms.Integration.ChildChangedEventArgs>(this.cartesianChart2_ChildChanged);
+            // 
+            // cartesianChart3
+            // 
+            this.cartesianChart3.Location = new System.Drawing.Point(392, 259);
+            this.cartesianChart3.Name = "cartesianChart3";
+            this.cartesianChart3.Size = new System.Drawing.Size(362, 177);
+            this.cartesianChart3.TabIndex = 11;
+            // 
+            // cartesianChart4
+            // 
+            this.cartesianChart4.Location = new System.Drawing.Point(790, 29);
+            this.cartesianChart4.Name = "cartesianChart4";
+            this.cartesianChart4.Size = new System.Drawing.Size(292, 170);
+            this.cartesianChart4.TabIndex = 12;
+            this.cartesianChart4.ChildChanged += new System.EventHandler<System.Windows.Forms.Integration.ChildChangedEventArgs>(this.cartesianChart4_ChildChanged);
+            // 
+            // cartesianChart5
+            // 
+            this.cartesianChart5.Location = new System.Drawing.Point(790, 248);
+            this.cartesianChart5.Name = "cartesianChart5";
+            this.cartesianChart5.Size = new System.Drawing.Size(321, 188);
+            this.cartesianChart5.TabIndex = 13;
+            this.cartesianChart5.ChildChanged += new System.EventHandler<System.Windows.Forms.Integration.ChildChangedEventArgs>(this.cartesianChart5_ChildChanged);
+            // 
+            // cartesianChart6
+            // 
+            this.cartesianChart6.Location = new System.Drawing.Point(12, 29);
+            this.cartesianChart6.Name = "cartesianChart6";
+            this.cartesianChart6.Size = new System.Drawing.Size(317, 170);
+            this.cartesianChart6.TabIndex = 14;
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(1199, 600);
+            this.Controls.Add(this.progressCpu);
+            this.Controls.Add(this.labelCpuUsage);
+            this.Controls.Add(this.progressRam);
+            this.Controls.Add(this.labelRamUsage);
+            this.Controls.Add(this.progressGpu);
+            this.Controls.Add(this.labelgGpuUsage);
+            this.Controls.Add(this.labelCpuTemp);
+            this.Controls.Add(this.labelGpuTemp);
+            this.Controls.Add(this.labelDiskUsage);
+            this.Controls.Add(this.cartesianChart1);
+            this.Controls.Add(this.cartesianChart2);
+            this.Controls.Add(this.cartesianChart3);
+            this.Controls.Add(this.cartesianChart4);
+            this.Controls.Add(this.cartesianChart5);
+            this.Controls.Add(this.cartesianChart6);
+            this.Name = "Form1";
+            this.ResumeLayout(false);
+
+        }
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,282 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient; // SQL Server için gerekli
+using System.Diagnostics;
+using System.IO;
+using System.Windows.Forms;
+using LibreHardwareMonitor.Hardware;
+using LiveCharts;
+using LiveCharts.Wpf;
+
+namespace SistemPerformansTakibi
+{
+    public partial class Form1 : Form
+    {
+        private PerformanceCounter cpuCounter;
+        private PerformanceCounter ramCounter;
+        private Computer computer;
+
+        private Timer dbTimer; // Veritabanı kaydı için timer
+
+        // 📌 **SQL Server Bağlantı Dizesi (Kendi Sunucana Göre Güncelle)**
+        private string connectionString = "Server=HATICENUR;Database=Performance;Trusted_Connection=True;MultipleActiveResultSets=true;TrustServerCertificate=True";
+
+        public Form1()
+        {
+            InitializeComponent();
+            // Performans sayaçlarını başlat
+            cpuCounter = new PerformanceCounter("Processor", "% Processor Time", "_Total");
+            ramCounter = new PerformanceCounter("Memory", "Available MBytes");
+
+            // LibreHardwareMonitor başlat
+            computer = new Computer
+            {
+                IsCpuEnabled = true,
+                IsGpuEnabled = true
+            };
+            computer.Open();
+
+            // Timer başlat, her saniye verileri güncelle
+            Timer timer = new Timer();
+            timer.Interval = 1000; // 1 saniye
+            timer.Tick += UpdateStats;
+            timer.Start();
+
+            // ✅ **10 saniyede bir SQL Server'a kayıt için yeni timer**
+            dbTimer = new Timer();
+            dbTimer.Interval = 10000; // 10 saniye
+            dbTimer.Tick += SaveToDatabase;
+            dbTimer.Start();
+
+            // Grafik yüklemek için veritabanı verilerini al
+            LoadData();
+        }
+
+        private void UpdateStats(object sender, EventArgs e)
+        {
+            // ✅ CPU Kullanımı
+            float cpuUsage = cpuCounter.NextValue();
+            progressCpu.Value = (int)cpuUsage;
+            labelCpuUsage.Text = $"CPU Kullanımı: {cpuUsage:F1}%";
+
+            // ✅ RAM Kullanımı
+            float totalRam = 16000; // Bilgisayardaki toplam RAM (MB olarak)
+            float usedRam = totalRam - ramCounter.NextValue();
+            float ramUsage = (usedRam / totalRam) * 100;
+            progressRam.Value = (int)ramUsage;
+            labelRamUsage.Text = $"RAM: {usedRam:F1} MB / {totalRam} MB";
+
+            // ✅ GPU Kullanımı
+            float gpuUsage = GetGpuUsage();
+            progressGpu.Value = (int)gpuUsage;
+            labelgGpuUsage.Text = $"GPU Kullanımı: {gpuUsage:F1}%";
+
+            // ✅ CPU ve GPU Sıcaklıkları
+            float cpuTemp = GetTemperature(HardwareType.Cpu);
+            float gpuTemp = GetTemperature(HardwareType.GpuNvidia) + GetTemperature(HardwareType.GpuAmd);
+
+            labelCpuTemp.Text = $"CPU Sıcaklığı: {cpuTemp} °C";
+            labelGpuTemp.Text = $"GPU Sıcaklığı: {gpuTemp} °C";
+
+            // ✅ Disk Kullanımı
+            DriveInfo drive = new DriveInfo("C");
+            long usedDisk = drive.TotalSize - drive.AvailableFreeSpace;
+            labelDiskUsage.Text = $"Disk: {usedDisk / 1024 / 1024 / 1024} GB / {drive.TotalSize / 1024 / 1024 / 1024} GB";
+        }
+
+        private float GetGpuUsage()
+        {
+            foreach (var hardware in computer.Hardware)
+            {
+                if (hardware.HardwareType == HardwareType.GpuNvidia || hardware.HardwareType == HardwareType.GpuAmd)
+                {
+                    hardware.Update();
+                    foreach (var sensor in hardware.Sensors)
+                    {
+                        if (sensor.SensorType == SensorType.Load && sensor.Name.Contains("GPU Core"))
+                        {
+                            Console.WriteLine($"GPU Kullanımı: {sensor.Value.GetValueOrDefault()}%");
+                            return sensor.Value.GetValueOrDefault();
+                        }
+                    }
+                }
+            }
+            return -1; // -1 dönerse GPU kullanımı okunamamış demektir.
+        }
+
+        private float GetTemperature(HardwareType type)
+        {
+            foreach (var hardware in computer.Hardware)
+            {
+                if (hardware.HardwareType == type)
+                {
+                    hardware.Update(); // Sensörleri güncelle
+                    foreach (var subHardware in hardware.SubHardware)
+                    {
+                        subHardware.Update(); // Alt donanımı güncelle
+                    }
+                    foreach (var sensor in hardware.Sensors)
+                    {
+                        if (sensor.SensorType == SensorType.Temperature)
+                        {
+                            float? value = sensor.Value;
+                            if (value.HasValue)
+                            {
+                                return value.Value; // Sıcaklık verisini döndür
+                            }
+                        }
+                    }
+                }
+            }
+            return -1; // Eğer sıcaklık değeri yoksa "-1" döndür
+        }
+
+        // ✅ **SQL Server'a her 10 saniyede bir veri kaydeden metod**
+        private void SaveToDatabase(object sender, EventArgs e)
+        {
+            try
+            {
+                float cpuUsage = cpuCounter.NextValue();
+                float totalRam = 16000;
+                float usedRam = totalRam - ramCounter.NextValue();
+                float ramUsage = (usedRam / totalRam) * 100;
+                float gpuUsage = GetGpuUsage();
+                float cpuTemp = GetTemperature(HardwareType.Cpu);
+                float gpuTemp = GetTemperature(HardwareType.GpuNvidia) + GetTemperature(HardwareType.GpuAmd);
+                DriveInfo drive = new DriveInfo("C");
+                long usedDisk = drive.TotalSize - drive.AvailableFreeSpace;
+
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+                    string query = "INSERT INTO PerformanceData (Tarih, CPU_Kullanimi, RAM_Kullanimi, GPU_Kullanimi, CPU_Sicaklik, GPU_Sicaklik, Disk_Kullanimi) VALUES (@Tarih, @CPU_Kullanimi, @RAM_Kullanimi, @GPU_Kullanimi, @CPU_Sicaklik, @GPU_Sicaklik, @Disk_Kullanimi)";
+
+                    using (SqlCommand command = new SqlCommand(query, connection))
+                    {
+                        command.Parameters.AddWithValue("@Tarih", DateTime.Now);
+                        command.Parameters.AddWithValue("@CPU_Kullanimi", cpuUsage);
+                        command.Parameters.AddWithValue("@RAM_Kullanimi", ramUsage);
+                        command.Parameters.AddWithValue("@GPU_Kullanimi", gpuUsage);
+                        command.Parameters.AddWithValue("@CPU_Sicaklik", cpuTemp);
+                        command.Parameters.AddWithValue("@GPU_Sicaklik", gpuTemp);
+                        command.Parameters.AddWithValue("@Disk_Kullanimi", usedDisk / 1024 / 1024 / 1024);
+
+                        command.ExecuteNonQuery();
+                    }
+                }
+
+                Console.WriteLine("✅ Veri başarıyla kaydedildi!");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("❌ Hata: " + ex.Message);
+            }
+        }
+
+
+        private void LoadData()
+        {
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                conn.Open();
+                string query = "SELECT Tarih, CPU_Kullanimi, RAM_Kullanimi, GPU_Kullanimi, CPU_Sicaklik, GPU_Sicaklik, Disk_Kullanimi FROM PerformanceData ORDER BY Tarih DESC";
+                SqlDataAdapter da = new SqlDataAdapter(query, conn);
+                DataTable dt = new DataTable();
+                da.Fill(dt);
+
+                // Verileri Grafiklere Ekle
+                ChartValues<double> cpuValues = new ChartValues<double>();
+                ChartValues<double> ramValues = new ChartValues<double>();
+                ChartValues<double> gpuValues = new ChartValues<double>();
+                ChartValues<double> cpuTempValues = new ChartValues<double>();
+                ChartValues<double> gpuTempValues = new ChartValues<double>();
+                ChartValues<double> diskValues = new ChartValues<double>();
+
+                foreach (DataRow row in dt.Rows)
+                {
+                    cpuValues.Add(Convert.ToDouble(row["CPU_Kullanimi"]));
+                    ramValues.Add(Convert.ToDouble(row["RAM_Kullanimi"]));
+                    gpuValues.Add(Convert.ToDouble(row["GPU_Kullanimi"]));
+                    cpuTempValues.Add(Convert.ToDouble(row["CPU_Sicaklik"]));
+                    gpuTempValues.Add(Convert.ToDouble(row["GPU_Sicaklik"]));
+                    diskValues.Add(Convert.ToDouble(row["Disk_Kullanimi"]));
+                }
+
+                // Grafik Serileri
+                cartesianChart1.Series = new SeriesCollection
+                {
+                    new LineSeries
+                    {
+                        Title = "CPU Kullanımı",
+                        Values = cpuValues
+                    }
+                };
+
+                cartesianChart2.Series = new SeriesCollection
+                {
+                    new LineSeries
+                    {
+                        Title = "RAM Kullanımı",
+                        Values = ramValues
+                    }
+                };
+
+                cartesianChart3.Series = new SeriesCollection
+                {
+                    new LineSeries
+                    {
+                        Title = "GPU Kullanımı",
+                        Values = gpuValues
+                    }
+                };
+
+                cartesianChart4.Series = new SeriesCollection
+                {
+                    new LineSeries
+                    {
+                        Title = "CPU Sıcaklığı",
+                        Values = cpuTempValues
+                    }
+                };
+
+                cartesianChart5.Series = new SeriesCollection
+                {
+                    new LineSeries
+                    {
+                        Title = "GPU Sıcaklığı",
+                        Values = gpuTempValues
+                    }
+                };
+
+                cartesianChart6.Series = new SeriesCollection
+                {
+                    new LineSeries
+                    {
+                        Title = "Disk Kullanımı",
+                        Values = diskValues
+                    }
+                };
+            }
+        }
+
+        private void cartesianChart1_ChildChanged(object sender, System.Windows.Forms.Integration.ChildChangedEventArgs e)
+        {
+
+        }
+
+        private void cartesianChart2_ChildChanged(object sender, System.Windows.Forms.Integration.ChildChangedEventArgs e)
+        {
+
+        }
+
+        private void cartesianChart5_ChildChanged(object sender, System.Windows.Forms.Integration.ChildChangedEventArgs e)
+        {
+
+        }
+
+        private void cartesianChart4_ChildChanged(object sender, System.Windows.Forms.Integration.ChildChangedEventArgs e)
+        {
+
+        }
+    }
+}
